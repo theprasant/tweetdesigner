8467e04cb45d591fba25dcb64aa979f8cc239e7f
prasantdev <prasantsahoorebel@gmail.com>
2022-06-05 17:29:18 +0530

    Ignore if someone mentions the bot under a tweet intended to use the bot

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
   username: "learnsjs",
-  userid: "1455810851168940039"
+  userid_str: "1455810851168940039"
 }
 
 export default config;
--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 import {getTweet} from './lib/twitter.js';
 
-let tweet = await getTweet('1531339927430090752');
-console.log(tweet);
+let tweet = await getTweet('1532150777568972800');
+console.log(JSON.stringify(tweet, null, 2));
