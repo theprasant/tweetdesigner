401ff055258282609098e27f2b2cf0f18d82a321
prasantdev <prasantsahoorebel@gmail.com>
2022-06-05 21:11:24 +0530

    fix emoji problem

--- test.js
+++ test.js
@@ -1,7 +1,28 @@
 import {getTweet, getMentions} from './lib/twitter.js';
+import drawTweetQuoteCanvas from './lib/canvas.js';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-// let tweet = await getTweet('1532150777568972800');
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// let tweet = await getTweet('1532700291325431809');
 // console.log(JSON.stringify(tweet, null, 2));
+import fs from 'fs';
+// 1200 x 630
+let tweetQuote = await drawTweetQuoteCanvas(1200, 630, {
+  // text: 'Lorem, ipsum dolor sit amet a.',
+  text: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab accusantium nobis odit impedit nostrum distinctio adipisci? Aliquid perspiciatis dolor inventore!`,
+  name: "Prasant kumar",
+  username: 'prasant123whatevern2',
+  pfp: 'https://cdn.discordapp.com/avatars/800445583046213663/027359d038895040a33236d17596d98f.png?size=4096',
+  followers: "-2.3 M",
+  followings: "+254",
+  fontArr: [path.join(__dirname, './assets/fonts/Baloo_Bhaijaan_2/BalooBhaijaan2-VariableFont_wght.ttf')],
+  returnFormat: 'buffer',
+  returnType: 'image/jpeg',
+  returnQuality: 1
+});
+fs.writeFileSync('./abcd.jpeg', tweetQuote)
 
 // let mentions = await getMentions('1455810851168940039', '2022-06-05T12:14:12.421Z');
 // console.log(mentions);
